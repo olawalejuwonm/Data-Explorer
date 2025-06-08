236f6d94b0831a3a9f39849b005ae6d4fa719eb7
Micheal Olawale <2437632@swansea.ac.uk>
2025-06-08 22:15:33 +0100

    Add data directory handling and update .gitignore for data files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // Entry point for Excel data merging CLI tool
 import * as XLSX from 'xlsx';
 import * as fs from 'fs';
+import * as path from 'path';
+
+const dataDir = path.join(__dirname, '../data');
+const files = fs.readdirSync(dataDir);
+console.log('Files in data folder:');
+files.forEach(file => console.log(file));
 
 console.log('Excel Data Merger CLI - To be implemented');
 // TODO: Implement CLI argument parsing, merging logic, and export functionality
